11ab802445385d5fce11d43fad2fdc1f1f51fa5e
Siddharth Nand <snand233@gmail.com>
2022-09-09 14:47:48 -0700

    added function comments

--- src/ui.py
+++ src/ui.py
@@ -4,6 +4,12 @@ from tkinter import *
 from tkinter import ttk
 
 class UI:
+    """
+        param root : object, Tk object
+        param title : string, window bar title
+        param w : real number, width of window
+        param h : real number, height of window
+    """
     def __init__(self, root, title, w, h):
         self.root = root
         self.title = title
@@ -14,5 +20,8 @@ class UI:
         frm = ttk.Frame(self.root, padding = 10)
         frm.grid()
 
+    """
+        SHOWS THE WINDOW
+    """
     def show(self):
         self.root.mainloop()
