17186839efc809acfea90155ce14dbc970f3c5df
Siddharth Nand <snand233@gmail.com>
2022-10-05 15:22:40 -0700

    added enums for each portfolio policy

--- src/enum_policy.py
+++ src/enum_policy.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class Policy(str, Enum):
-    EW = 'ew'
-    MINIMUN_VAR = 'minvar'
-    MINIMUM_VAR_CONSTRAINED = 'minvar-constrained'
+    EW = 'Equal Weight'
+    MINIMUM_VAR = 'Minimum Variance'
+    MINIMUM_VAR_CONSTRAINED = 'Minimum Variance with Shortsell Constraints'
+    MINIMUM_VAR_GENERALIZED_CONSTRAINED = 'Minimum Variance with General Constraints'
