b0d463fe0d8f70cece0254f7ccfdde14c9d639c3
Siddharth Nand <snand233@gmail.com>
2022-11-11 22:14:43 -0800

    began cleaning code so we have a class for each model

--- src/enum_policy.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class Policy(str, Enum):
-    EW = 'Equal Weight'
-    MINIMUM_VAR = 'Minimum Variance'
-    MINIMUM_VAR_CONSTRAINED = 'Minimum Variance with Shortsell Constraints'
-    MINIMUM_VAR_GENERALIZED_CONSTRAINED = 'Minimum Variance with General Constraints'
-    KAN_ZHOU_EW = "Kan-Zhou Equal Weight"
