2b8b97623177b55e29060dce196378c892f9634d
Siddharth Nand <snand233@gmail.com>
2022-11-14 18:53:21 -0800

    editted description

--- src/models/ew.py
+++ src/models/ew.py
@@ -5,8 +5,8 @@ from .model import Model
 class EqualWeightModel(Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._name = 'Equal Weight'
-        self._description = 'Equal Weight Model'
+        self._name = 'EqW'
+        self._description = '1 / N Equal Weight Portfolio'
 
     def _run(self, cols):
         return 1/cols * np.ones((cols - 1, 1))
