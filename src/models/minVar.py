59a9dbd871c2cf0f7f259c5d5e179313006d7650
Siddharth Nand <snand233@gmail.com>
2022-11-14 18:54:02 -0800

    added description

--- src/models/minVar.py
+++ src/models/minVar.py
@@ -5,7 +5,8 @@ from .model import Model
 class MinVariance(Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._name = 'Min Variance'
+        self._name = 'Min Var'
+        self._description = 'Minimum Variance Portfolio'
 
     def _run(self, invSigmaMLE, AMLE, cols):
         return 1/AMLE * np.dot(invSigmaMLE, np.ones((cols - 1, 1)))
