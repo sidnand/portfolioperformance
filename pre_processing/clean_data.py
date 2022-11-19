5bff480cab55f06d344b43f8d7a46684faced5ae
Siddharth Nand <snand233@gmail.com>
2022-11-18 20:47:40 -0800

    cleaned sp_sectors data

--- pre_processing/clean_data.py
+++ pre_processing/clean_data.py
@@ -1,8 +1,13 @@
+import os
 import numpy as np
 
 DATE = 0
 OPEN = 1
-ADJ_CLOSE = 5 
+ADJ_CLOSE = 5
+
+scriptDir = os.path.dirname(__file__)
+relReadPath = "../data/new/orig"
+relWritePath = "../data/new/clean"
 
 def readData(filename):
     # read csv file
@@ -12,3 +17,27 @@ def readData(filename):
 def percentageChange(x, y):
     # return percentage change
     return (y - x) / x
+
+def processData(data):
+    newData = percentageChange(data[:, OPEN], data[:, ADJ_CLOSE])
+    newData = np.column_stack((data[:, DATE], newData))
+    return newData
+
+def processSector(readPath, sector, writePath):
+    indir = os.path.join(scriptDir, readPath, sector)
+    outdir = os.path.join(scriptDir, writePath, sector)
+
+    files = os.listdir(indir)
+    # process each file
+    for file in files:
+        # read data
+        data = readData(indir + "/" + file)
+        # process data
+        data = processData(data)
+        # create new file and save data
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+
+        np.savetxt(outdir + "/" + file, data, delimiter=',', fmt='%s', header="date,change", comments='')
+
+processSector(relReadPath, "sp_sector", relWritePath)
--- pre_processing/get_data.py
+++ pre_processing/get_data.py
@@ -1,27 +1,35 @@
+import os
 import yfinance as yf
 
-def get_SPSector():
-    T_BILL_3_MO = "%" + "5EIRX"
-    SP_FINANCE = "%" + "5ESP500-40"
-    SP_ENEGY = "%" + "5EGSPE"
-    SP_MATERIALS = "%" + "5ESP500-15"
-    SP_CONSUM_DIS = "%" + "5ESP500-25"
-    SP_CONSUM_STAPLE = "%" + "5ESP500-30"
-    SP_HEALTH = "%" + "5ESP500-35"
-    SP_UTIL = "%" + "5ESP500-55"
-    SP_500 = "%" + "5EGSPC"
-    SP_INFO_TECH = "%" + "5ESP500-45"
-    SP_TELE_COMM = "%" + "5ESP500-50"
+scriptDir = os.path.dirname(__file__)
+relWritePath = "../data/new/orig"
 
-    SYMBOLS = [T_BILL_3_MO, SP_FINANCE, SP_ENEGY, SP_MATERIALS, SP_CONSUM_DIS, SP_CONSUM_STAPLE, SP_HEALTH, SP_UTIL, SP_500, SP_INFO_TECH, SP_TELE_COMM]
+sp_sectors = {
+    "T_BILL_3_MO" : "%5EIRX",
+    "SP_FINANCE" : "%5ESP500-40",
+    "SP_ENEGY" : "%5EGSPE",
+    "SP_MATERIALS" : "%5ESP500-15",
+    "SP_CONSUM_DIS" : "%5ESP500-25",
+    "SP_CONSUM_STAPLE" : "%5ESP500-30",
+    "SP_HEALTH" : "%5ESP500-35",
+    "SP_UTIL" : "%5ESP500-55",
+    "SP_500" : "%5EGSPC",
+    "SP_INFO_TECH" : "%5ESP500-45",
+    "SP_TELE_COMM" : "%5ESP500-50"
+}
 
-    return SYMBOLS
+industries = {}
 
-def download(symbols, interval, start, end):
-    for ticker in symbols:
-        data = yf.download(ticker, group_by="Ticker", interval=interval, start=start, end=end)
-        data.to_csv(f'ticker_{ticker}.csv')  # ticker_AAPL.csv for example
+def download(symbols, interval, start, end, path, folderName):
+    outdir = os.path.join(scriptDir, path) + "/" + folderName
+
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    for key, val in symbols.items():
+        data = yf.download(val, group_by="Ticker", interval=interval, start=start, end=end)
+        data.index = data.index.strftime("%Y%m%d")
+        data.to_csv(outdir + "/" + key + ".csv")
 
 # download sp500 sector data
-symbols = get_SPSector()
-download(symbols, "1mo", "2001-01-01", "2021-01-01")
+download(sp_sectors, "1mo", "2001-01-01", "2021-01-01", relWritePath, "sp_sector")
