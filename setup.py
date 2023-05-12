3775362394603bac6a4450cef2e1c36e7cde0508
Siddharth Nand <snand233@gmail.com>
2023-05-12 01:30:16 -0700

    changed package name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='portfolio_performance',
-    version='1.0.4',
+    name='portfolio-performance',
+    version='1.0.0',
     author='Siddharth Nand',
     author_email='snand233@gmail.com',
     install_requires=[
