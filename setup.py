b4bc9f5fda29889ec836e828c2ffbbba6f9daf3d
Siddharth Nand <snand233@gmail.com>
2023-05-14 13:55:29 -0700

    improved setup

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='portfolioperformance',
     version='1.0.0',
     author='Siddharth Nand',
     author_email='snand233@gmail.com',
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scipy',
-        'quadprog',
-        'qpsolvers'
+    
+    url="https://github.com/sidnand/portfolioperformance",
+
+    description="Tool to test the out-of-sample performance of portfolio optimization models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(),
+    
+    license="GNU",
+    license_files=["LICENSE.txt"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ]
 )
